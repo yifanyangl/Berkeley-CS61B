c8227bbef194a22b92c110f2d42a390cc1df5eda
Kai Liao <38992157+yifanyangl@users.noreply.github.com>
2019-01-27 18:06:33 -0500

    [proj1b] Completes fun parts of task 5

--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -1,18 +1,59 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 /**
  * This class outputs all palindromes in the words file in the current directory.
  */
 public class PalindromeFinder {
 
     public static void main(String[] args) {
-        int minLength = 4;
-        In in = new In("../library-sp18/data/words.txt");
-        Palindrome palindrome = new Palindrome();
+        int minOffset = 1;
+        int maxOffset = 25;
+        int minLength = 2;
 
+        In in = new In("../library-sp18/data/words.txt");
+        List<String> allWords = new ArrayList<>();
         while (!in.isEmpty()) {
-            String word = in.readString();
-            if (word.length() >= minLength && palindrome.isPalindrome(word, new OffByN(2))) {
-                System.out.println(word);
+            allWords.add(in.readString());
+        }
+
+        Map<Integer, List<String>> allPalindromes = new LinkedHashMap<>();
+        for (int n = minOffset; n < maxOffset + 1; n++) {
+            List<String> found = findPalindromesOffByN(n, minLength, allWords);
+            if (!found.isEmpty()) {
+                allPalindromes.put(n, found);
             }
         }
+
+        Entry<Integer, List<String>> mostPalindrome = allPalindromes.entrySet().stream()
+            .max((e1, e2) -> (e1.getValue().size() - e2.getValue().size()))
+            .orElseThrow(() -> new IllegalStateException("No palindromes found"));
+        System.out.println(String.format("Most palindromes: N = %d, number = %s\n",
+            mostPalindrome.getKey(), mostPalindrome.getValue().size()));
+
+        String longestPalin = allPalindromes.values().stream()
+            .map(lst -> lst.stream()
+                .max(Comparator.comparingInt(String::length))
+                .orElseThrow(() -> new IllegalStateException("No palindromes found")))
+            .max(Comparator.comparingInt(String::length))
+            .orElseThrow(() -> new IllegalStateException("No palindromes found"));
+        System.out.println(String.format("Longest palindromes for any N: %s", longestPalin));
+    }
+
+    private static List<String> findPalindromesOffByN(int N, int minLength, List<String> allWords) {
+        Palindrome palindrome = new Palindrome();
+
+        List<String> result = new ArrayList<>();
+        for (String word : allWords) {
+            if (word.length() >= minLength && palindrome.isPalindrome(word, new OffByN(N))) {
+                result.add(word);
+            }
+        }
+
+        return result;
     }
 }
