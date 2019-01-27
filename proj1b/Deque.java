2868fa3ab5388b6db8e4b7bec7796e6be3336320
Kai Liao <38992157+yifanyangl@users.noreply.github.com>
2019-01-27 14:00:13 -0500

    [proj1b] Completes Task 1

---
+++ proj1b/Deque.java
@@ -0,0 +1,20 @@
+public interface Deque<T> {
+
+    void addFirst(T item);
+
+    void addLast(T item);
+
+    boolean isEmpty();
+
+    int size();
+
+    void printDeque();
+
+    T removeFirst();
+
+    T removeLast();
+
+    T get(int index);
+
+    T getRecursive(int index);
+}
